dfbda8cb9a386b5cc8b3605fcd12619079281634
imndy <159553753+imndy@users.noreply.github.com>
2025-06-10 01:18:24 +0700

    Update send_to_discord.sh

--- send_to_discord.sh
+++ send_to_discord.sh
@@ -2,33 +2,51 @@
 
 OUTPUT=$(python3 -c "
 import nbformat
-import json
 import pandas as pd
+import json
 from bs4 import BeautifulSoup
 
 nb = nbformat.read(open('output.ipynb'), as_version=4)
 cells = [c for c in nb.cells if c.cell_type == 'code' and 'outputs' in c]
 
-if not cells or not cells[-1]['outputs']:
-    print('[No output found]')
-else:
-    out = cells[-1]['outputs'][0]
-    if out['output_type'] == 'display_data' and 'text/html' in out['data']:
-        html = out['data']['text/html']
+def extract_df_from_html(html):
+    try:
         soup = BeautifulSoup(html, 'html.parser')
         table = soup.find('table')
         if table:
             df = pd.read_html(str(table))[0]
-            print(df.to_markdown(index=False))
+            return df.to_markdown(index=False)
+    except Exception as e:
+        return f'[Failed to parse HTML table: {e}]'
+    return None
+
+if not cells or not cells[-1]['outputs']:
+    print('[No output found]')
+else:
+    out = cells[-1]['outputs'][0]
+    if out['output_type'] == 'execute_result':
+        data = out.get('data', {})
+        if 'text/plain' in data:
+            print(data['text/plain'])
+        elif 'text/html' in data:
+            print(extract_df_from_html(data['text/html']))
         else:
-            print('[No HTML table found]')
-    elif 'text/plain' in out.get('data', {}):
-        print(out['data']['text/plain'])
+            print('[No printable data]')
+    elif out['output_type'] == 'display_data':
+        data = out.get('data', {})
+        if 'text/html' in data:
+            print(extract_df_from_html(data['text/html']))
+        elif 'text/plain' in data:
+            print(data['text/plain'])
+        else:
+            print('[No printable display data]')
+    elif out['output_type'] == 'stream':
+        print(out.get('text', '[No stream text]'))
     else:
         print('[Unsupported output format]')
 ")
 
-# escape for Discord
+# Escape backslashes and quotes for JSON
 ESCAPED_OUTPUT=$(echo "$OUTPUT" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g')
 
 curl -X POST "$WEBHOOK_URL" \
